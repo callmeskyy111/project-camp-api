edbe37ba4ed75168e62f67acb555db52a52a4b0b
Soumadip <39thdecember@gmail.com>
2025-09-11 23:22:51 +0530

    Add CORS-config ✅

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get('/',(_,res)=>{
-    res.send("Welcome to HomePage ✅")
-})
+// middlewares
+app.use(express.json({ limit: "16kb" })); // limit - Optnl.
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // serving static files.
+
+app.use(
+  // cors config.
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/", (_, res) => {
+  res.send("Welcome to HomePage ✅");
+});
 
 export default app;
