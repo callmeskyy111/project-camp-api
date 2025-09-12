dbc450b5da2b4e7312ebd014ecd9d583e2617815
Soumadip <39thdecember@gmail.com>
2025-09-12 19:23:43 +0530

    Add User Model/Schema 👤✅

---
+++ src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    avatar: {
+      type: { url: String, localPath: String },
+      default: {
+        url: ``,
+        localPath: `https://placehold.co/200x200`,
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "🔴 Password is required!"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel =
+  mongoose.models.User || mongoose.model("User", userSchema);
+
+export default UserModel;
