a49638caa63269adc11ad8b64ec6b68e9ef7c5b9
Soumadip <39thdecember@gmail.com>
2025-09-12 19:32:17 +0530

    Add Password-Hashing pre-hook( ) ✅

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -56,7 +57,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const UserModel =
-  mongoose.models.User || mongoose.model("User", userSchema);
+// hashing pre-hook()
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next; // only execute when password is being modified.
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default UserModel;
