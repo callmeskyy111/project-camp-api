837dcc3ac7c6c64095aaa1ff0ff09a4e60f9b392
Soumadip <39thdecember@gmail.com>
2025-09-13 21:48:11 +0530

    Create Access and Refresh Token ☑️

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,9 +1,7 @@
-// Wrapper f(x) , H.O.F () - Ni more try/catch
+// Wrapper f(x) , H.O.F () - No more try/catch
 
 export function asyncHandler(requestHanlder) {
   return (req, res, next) => {
-    Promise.resolve(requestHanlder(req, res, next))
-      .catch((err) => next(err))
-      .catch((err) => next(err));
+    Promise.resolve(requestHanlder(req, res, next)).catch((err) => next(err));
   };
 }
